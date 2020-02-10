e6a6780c15847c757282394cccee82bc4ff435a0
akups <appiah-lindberg@outlook.com>
2020-02-10 19:39:12 +0100

    ctreating models

--- models/User.js
+++ models/User.js
@@ -1,15 +1,22 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String,
-  password: String
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
+  firstName: String,
+  lastName: String,
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessCode: {
+    type: String,
+    required: true
   }
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
