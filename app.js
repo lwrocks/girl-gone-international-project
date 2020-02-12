7c342a1fe2682952a8c7ceca94b0811eb639bf3d
akups <appiah-lindberg@outlook.com>
2020-02-12 14:44:31 +0100

    debug module

--- app.js
+++ app.js
@@ -14,9 +14,12 @@ const MongoStore = require("connect-mongo")(session);
 const flash = require("connect-flash");
 
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/girl-gone-international", {
-    useNewUrlParser: true
-  })
+  .connect(
+    process.env.MONGODB_URI || "mongodb://localhost/girl-gone-international",
+    {
+      useNewUrlParser: true
+    }
+  )
   .then(x => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
